87633e2f3e0b43a0b0ccd801870961f24704c770
RexyCloudOfShit <rexycloudy63@gmail.com>
2025-10-21 14:47:33 -0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,1 @@
-local Games = {
-  OperationOne = 72920620366355,
-  Rost = 13769775130 or 122308715301969,
-  Vally = 15784744207,
-  Fallen = 10228136016,
-  ProjectDelta = 7336302630,
-  Unnamed = 14202073004,
-  Entrenched = 3678761576,
-  AP = 10518166490
-}
+local Games = { OperationOne = 72920620366355, Rost = 13769775130, Vally = 15784744207, Fallen = 10228136016, ProjectDelta = 7336302630, Unnamed = 14202073004, Entrenched = 3678761576, AP = 10518166490 } for name, id in pairs(Games) do if game.PlaceId == id then loadstring(game:HttpGet("https://raw.githubusercontent.com/RexyCloudOfShit/SigmaCheat/main/" .. name))() break end end
