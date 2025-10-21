070b0cdce522076c2f0bd46904fe9bb348baae43
RexyCloudOfShit <rexycloudy63@gmail.com>
2025-10-21 14:42:28 -0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
-print("yes loaded")
+local Games = {
+  OperationOne = 72920620366355,
+  Rost = 13769775130 or 122308715301969,
+  Vally = 15784744207,
+  Fallen = 10228136016,
+  ProjectDelta = 7336302630,
+  Unnamed = 14202073004,
+  Entrenched = 3678761576,
+  AP = 10518166490
+}
